9de1449e040e1dbaf57dd2a1322fc422dd7ab2e7
koltrastH <elina.hulbert@hyperisland.se>
2022-03-31 19:28:13 +0200

    experience, services, portfolio components

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,10 +1,123 @@
-import React from 'react';
+import React from "react";
 import "./Experience.css";
+import {BsPatchCheckFill} from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {/* #1 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+              
+            </article>
+            {/* #2 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #3 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #4 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #5 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #6 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            {/* #1 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #2 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #3 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #4 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #5 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #6 */}
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React from "react";
 import "./Portfolio.css";
+import img1 from "../../assets/portfolio/1.png";
+import img2 from "../../assets/portfolio/2.png";
+import img3 from "../../assets/portfolio/3.png";
+import img4 from "../../assets/portfolio/4.png";
+import img5 from "../../assets/portfolio/5.png";
+import img6 from "../../assets/portfolio/6.png";
+
+const data = [
+  {
+    id: 1,
+    image: img1,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "First",
+    github: "https://github.com",
+    demo: "https://www.instagram.com/p/B6Wa_VqDFcA/",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
