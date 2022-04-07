b7607e26f43138fc5a959ed1d2cf84af273088ae
koltrastH <elina.hulbert@hyperisland.se>
2022-04-07 23:18:32 +0200

    general skills added
     Styles on skills section were fixed. Responsiveness fixed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -13,232 +13,342 @@ const Experience = () => {
           <div className="experience__content">
             {/* #1 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
                 <h4>Vanilla JavaScript</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light">
+                  Local & session storage, API's, quiz, Date object
+                </small>
               </div>
             </article>
             {/* #2 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>CSS</h4>
-                <small className="text-light">Experienced</small>
+                <h4>MongoDB, Node.js & Express</h4>
+                <small className="text-light">MERN briefs</small>
               </div>
             </article>
             {/* #3 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
                 <h4>React.js</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light">
+                  MERN briefs, calculator, API's
+                </small>
               </div>
             </article>
             {/* #4 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>MongoDB & Node.js</h4>
-                <small className="text-light">Experienced</small>
+                <h4>Firebase</h4>
+                <small className="text-light">
+                  Quiz, Firebase Authentication
+                </small>
               </div>
             </article>
             {/* #5 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>Firebase</h4>
-                <small className="text-light">Experienced</small>
+                <h4>CSS</h4>
+                <small className="text-light">
+                  SCSS/SASS, Bootstrap, Tailwind, Flex, Grid
+                </small>
               </div>
             </article>
             {/* #6 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
                 <h4>Phaser</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            {/* #7 */}
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Accessibility</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light">Seabattle game</small>
               </div>
             </article>
             {/* #8 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>Git, GitHub</h4>
+                <h4>Responsive Web Design & Accessibility</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #9 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>Responsive Web Design</h4>
-                <small className="text-light">Experienced</small>
+                <h4>Git, GitHub</h4>
               </div>
             </article>
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Frontend Development</h3>
+        <div className="experience__frontend">
+          <h3>Art</h3>
           <div className="experience__content">
             {/* #1 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Typography Composition</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #2 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Design for the User</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #3 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Coloristics</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #4 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Illustration </h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #5 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Digital Art</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #6 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>3D Modelling</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #7 */}
+            <article className="experience__details">
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
+              <div>
+                <h4>Painting</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #8 */}
+            <article className="experience__details">
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
+              <div>
+                <h4>Architectural Drawing</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Frontend Development</h3>
+        <div className="experience__frontend">
+          <h3>Software</h3>
           <div className="experience__content">
             {/* #1 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Figma</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #2 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Adobe Illustrator</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #3 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Adobe Photoshop</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #4 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Adobe Indesign</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #5 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Trello</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #6 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Miro</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            {/* #7 */}
+            <article className="experience__details">
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
+              <div>
+                <h4>Autodesk Maya</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Backend Development</h3>
+        <div className="experience__frontend">
+          <h3>Soft Skills</h3>
           <div className="experience__content">
             {/* #1 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Fast Learner</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #2 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Ability To Work In a Team</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #3 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Patience</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #4 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Accuracy</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #5 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Social Skills</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             {/* #6 */}
             <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+              <BsPatchCheckFill
+                size={13}
+                className="experience__details-icon"
+              />
               <div>
-                <h4>HTML</h4>
+                <h4>Flexibility & Adaptibility</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
