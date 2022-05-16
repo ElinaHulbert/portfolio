aa53fd6b5149e761a46821d314e4dfed3b1dc78d
koltrastH <elina.hulbert@hyperisland.se>
2022-05-16 19:57:31 +0200

    added two more projects and updated github repo buttons

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,13 +12,15 @@ import img9 from "../../assets/portfolio/9.png";
 import img10 from "../../assets/portfolio/10.png";
 import img11 from "../../assets/portfolio/11.png";
 import img12 from "../../assets/portfolio/12.png";
+import img13 from "../../assets/portfolio/13.png";
+import img14 from "../../assets/portfolio/14.png";
 
 const data = [
   {
     id: 1,
     image: img1,
     title: "Mogo Ready Template",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/template-layout",
     demo: "https://elinahulbert.github.io/template-layout/",
     description:
       "A company portfolio written with HTML, CSS and jQuery using a ready psd template.",
@@ -27,7 +29,7 @@ const data = [
     id: 2,
     image: img2,
     title: "React Calculator",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/calculatorReact",
     demo: "https://react-app-calc20.herokuapp.com/",
     description: "Minimalistic React Calculator with Clear function.",
   },
@@ -35,7 +37,7 @@ const data = [
     id: 3,
     image: img3,
     title: "Sweet Portfolio",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/Lyudmila-Yakovleva",
     demo: "https://elinahulbert.github.io/Lyudmila-Yakovleva/index.html",
     description: "The very first project in vanilla JS. A portfolio for baker.",
   },
@@ -43,7 +45,7 @@ const data = [
     id: 4,
     image: img4,
     title: "Bootstrap in React",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/sign-in-form",
     demo: "https://react-app-bootstrap2738.herokuapp.com/",
     description: "A simple Sign In page made with Bootstrap.",
   },
@@ -51,7 +53,7 @@ const data = [
     id: 5,
     image: img5,
     title: "Seabattle",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/seabattle",
     demo: "https://elinahulbert.github.io/seabattle/",
     description:
       "A classic Seabattle/Battleship game for one person made in Phaser.",
@@ -60,7 +62,7 @@ const data = [
     id: 6,
     image: img6,
     title: "Holiday Counter",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/holiday-countdown",
     demo: "https://rococo-starlight-64da6d.netlify.app/",
     description:
       "The first team project to keep track on favourite sweet holidays using localstorage.",
@@ -69,7 +71,7 @@ const data = [
     id: 7,
     image: img7,
     title: "Quiz",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/Twinder",
     demo: "https://elinahulbert.github.io/Twinder/",
     description:
       "The second team project with login & register page using Firebase.",
@@ -77,16 +79,16 @@ const data = [
   {
     id: 8,
     image: img8,
-    title: "Bootstrap in Vanilla JS",
-    github: "https://github.com",
+    title: "Bootstrap",
+    github: "https://github.com/ElinaHulbert/bootstrap",
     demo: "https://brilliant-sorbet-c8f5e5.netlify.app",
-    description: "Slider made with Bootstrap and vanilla JS.",
+    description: "Slider made with Bootstrap.",
   },
   {
     id: 9,
     image: img9,
     title: "Responsive Web",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/responsiveWeb",
     demo: "https://unique-cobbler-e87ff0.netlify.app/",
     description:
       "A responsive website with media queries adjusted to printing.",
@@ -95,7 +97,7 @@ const data = [
     id: 10,
     image: img10,
     title: "MERN Sneakers",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/react-sneakers",
     demo: "https://unique-bombolone-cb639e.netlify.app/",
     description:
       "A sneakers MERN web with favourite feature deployed as a monorepo in Heroku and Netlify.",
@@ -104,7 +106,7 @@ const data = [
     id: 11,
     image: img11,
     title: "Vanilla JS",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/Vanilla-JS-projects",
     demo: "https://kaleidoscopic-ganache-2f7410.netlify.app/",
     description: "Several vanilla JS projects as a part of JavaScript30.",
   },
@@ -112,9 +114,26 @@ const data = [
     id: 12,
     image: img12,
     title: "Swedish Radio API",
-    github: "https://github.com",
+    github: "https://github.com/ElinaHulbert/Swedish-radio-API",
     demo: "https://elinahulbert.github.io/Swedish-radio-API/",
     description: "A Vanilla JS App based on Swedish Radio API.",
+  },
+  {
+    id: 13,
+    image: img13,
+    title: "MERN Experience",
+    github: "https://github.com/ElinaHulbert/ecommerce-team-project",
+    demo: "https://benciantar-happening.netlify.app/",
+    description:
+      "A team MERN project deployed as a Monorepo on Heroku and Netlify.",
+  },
+  {
+    id: 14,
+    image: img14,
+    title: "Swedish Radio API with React",
+    github: "https://github.com/ElinaHulbert/first_web_app",
+    demo: "https://sparkling-douhua-342792.netlify.app/",
+    description: "A React App based on Swedish Radio API.",
   },
 ];
 
